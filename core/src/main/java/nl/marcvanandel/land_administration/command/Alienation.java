d25dbf7352be1142f1f5eb4bc50f96b3bf0e0e7f
andelm <Marc.vanAndel@kadaster.nl>
2017-02-27 15:12:24 +0100

    first set up of the land administration model

---
+++ core/src/main/java/nl/marcvanandel/land_administration/command/Alienation.java
@@ -0,0 +1,9 @@
+package nl.marcvanandel.land_administration.command;
+
+/**
+ * // TODO
+ *
+ * @author Marc van Andel
+ * @since 0.1 on 27-2-2017
+ */
+public class Alienation {}
---
+++ core/src/main/java/nl/marcvanandel/land_administration/domain/model/Parcel.java
@@ -0,0 +1,34 @@
+package nl.marcvanandel.land_administration.domain.model;
+
+import nl.marcvanandel.land_administration.command.Alienation;
+import nl.marcvanandel.land_administration.domain.event.RightTransferedEvent;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.commandhandling.model.AggregateRoot;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+
+import java.util.Set;
+
+/**
+ * A Parcel represents an area in a country, a piece of land. To this all kind of rights and taxes are bound.
+ *
+ * @author Marc van Andel
+ * @since 0.1 on 27-2-2017
+ */
+@AggregateRoot
+public class Parcel {
+
+    @AggregateIdentifier
+    private Long identifier;
+    private String location;
+    private Set<Right> rights;
+
+    public void transfer(Alienation alienation) {
+
+    }
+
+    @EventSourcingHandler
+    protected void handle(RightTransferedEvent event) {
+
+    }
+
+}
