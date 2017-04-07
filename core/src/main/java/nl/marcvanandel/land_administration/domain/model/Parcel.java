c946e0cf6d5b2884ab784f07ba8bac48402750c2
andelm <Marc.vanAndel@kadaster.nl>
2017-04-07 11:39:58 +0200

    parcel as aggregate

--- core/src/main/java/nl/marcvanandel/land_administration/domain/model/Parcel.java
+++ core/src/main/java/nl/marcvanandel/land_administration/domain/model/Parcel.java
@@ -3,9 +3,14 @@ package nl.marcvanandel.land_administration.domain.model;
 import nl.marcvanandel.land_administration.command.Alienation;
 import nl.marcvanandel.land_administration.domain.datatype.ParcelId;
 import nl.marcvanandel.land_administration.domain.event.RightTransferedEvent;
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
 import org.axonframework.commandhandling.model.AggregateRoot;
+import org.axonframework.commandhandling.model.inspection.AggregateModel;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventsourcing.EventSourcedAggregate;
 import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.eventsourcing.SnapshotTrigger;
 
 import java.util.Set;
 
@@ -16,20 +21,33 @@ import java.util.Set;
  * @since 0.1 on 27-2-2017
  */
 @AggregateRoot
-public class Parcel {
+public class Parcel extends EventSourcedAggregate<Parcel> {
 
     @AggregateIdentifier
     private ParcelId identifier;
     private String location;
     private Set<Right> rights;
 
-    public void transfer(Alienation alienation) {
+    protected Parcel(
+        AggregateModel<Parcel> model, EventBus eventBus, SnapshotTrigger snapshotTrigger
+    ) {
+        super(model, eventBus, snapshotTrigger);
+    }
 
+    @CommandHandler
+    public void transfer(Alienation alienation) {
+        // handle command
+        apply(new RightTransferedEvent());
     }
 
     @EventSourcingHandler
     protected void handle(RightTransferedEvent event) {
+        // update state
+    }
 
+    @Override
+    public String identifierAsString() {
+        return identifier.toString();
     }
 
 }
